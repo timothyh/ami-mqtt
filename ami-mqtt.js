e602744e233fe1840d2ae19a35ea11a3dafb36c7
Timothy Hunt <tim-github.com@timhunt.net>
2017-04-08 15:13:38 -0400

    Add support for trunk and extension status changes Tracks when a call is answered and is hungup

--- ami-mqtt.js
+++ ami-mqtt.js
@@ -35,14 +35,16 @@
 // 
 //
 
-String.prototype.inList = function ( list ) {
-    return ( list.indexOf( this.toString() ) != -1 )
+String.prototype.inList = function(list) {
+    return (list.indexOf(this.toString()) != -1)
 }
 
-require('log-timestamp')(function() { return new Date().toString() + ": %s" });
+require('log-timestamp')(function() {
+    return new Date().toString() + ": %s"
+});
 
-var namiLib = require( "nami" )
-var mqtt = require( "mqtt" )
+var namiLib = require("nami")
+var mqtt = require("mqtt")
 
 var config = require('./config.json');
 
@@ -54,48 +56,52 @@ var mqtt_conf = config.mqtt_conf
 
 var trunks = config.trunks
 
-var callers = { }
+var callers = {}
+
+var calls = {}
 
 var ami_activity = Date.now()
 var mqtt_activity = Date.now()
 
-var client = mqtt.connect( {
+var client = mqtt.connect({
     host: mqtt_conf.host,
     port: mqtt_conf.port,
     username: mqtt_conf.username,
     password: mqtt_conf.password,
     keepalive: mqtt_conf.keepalive
-} )
+})
 
-client.on( 'connect', function () {
-    console.log( "Connected to MQTT Broker" )
-    client.subscribe( mqtt_conf.ping_topic )
-} )
+client.on('connect', function() {
+    console.log("Connected to MQTT Broker")
+    client.subscribe(mqtt_conf.ping_topic)
+})
 
-var nami = new( namiLib.Nami )( ami_conf )
+var nami = new(namiLib.Nami)(ami_conf)
 
 nami.logLevel = 1
 
-nami.on('namiConnectionClose', function (data) {
+nami.on('namiConnectionClose', function(data) {
     console.warn('Reconnecting to AMI...');
-    setTimeout(function () { nami.open(); }, 5000);
+    setTimeout(function() {
+        nami.open();
+    }, 5000);
 });
 
-nami.on('namiInvalidPeer', function (data) {
-        console.error("Invalid AMI Salute. Not an AMI?");
-        process.exit(1);
+nami.on('namiInvalidPeer', function(data) {
+    console.error("Invalid AMI Salute. Not an AMI?");
+    process.exit(1);
 });
-nami.on('namiLoginIncorrect', function () {
-        console.error("Invalid AMI Credentials");
-        process.exit(1);
+nami.on('namiLoginIncorrect', function() {
+    console.error("Invalid AMI Credentials");
+    process.exit(1);
 });
-nami.on( 'namiEvent', function ( event ) {
+nami.on('namiEvent', function(event) {
     ami_activity = Date.now()
 
-    if ( !event.event.inList( [ 'NewConnectedLine', 'DialBegin', 'DialEnd', 'Hangup' ] ) ) {
+    if (!event.event.inList(['NewConnectedLine', 'DialBegin', 'DialEnd', 'Hangup'])) {
         return
     }
-    if ( !event.channelstatedesc.inList( [ 'Down', 'Rsrvd', 'Ring', 'Ringing', 'Up' ] ) ) {
+    if (!event.channelstatedesc.inList(['Down', 'Rsrvd', 'Ring', 'Ringing', 'Up'])) {
         return
     }
 
@@ -114,91 +120,156 @@ nami.on( 'namiEvent', function ( event ) {
 
     delete event.lines
     delete event.EOL
-    console.log( util.inspect(event) )
+    console.log(util.inspect(event))
 
-    // New incoming call
-    if ( event.event === 'NewConnectedLine' &&
+    // Process New incoming call
+    // Trunk incoming
+    if (event.event === 'NewConnectedLine' &&
         event.channelstatedesc === 'Ring' &&
-        trunks[ event.exten ] ) {
-	callers[event.calleridnum] = event.calleridname.replace( /[_\s]+/g, ' ' )
-        var trunk = trunks[ event.exten ].toLowerCase()
+        trunks[event.exten]) {
+        callers[event.calleridnum] = event.calleridname.replace(/[_\s]+/g, ' ')
+        var trunk = trunks[event.exten].toLowerCase()
         var tmp = {
-            trunk: trunks[ event.exten ],
+            trunk: trunks[event.exten],
             to_num: event.exten,
             from_num: event.calleridnum === 'Unavailable' ? '' : event.calleridnum,
-            from_name: event.calleridname.replace( /[_\s]+/g, ' ' ),
+            from_name: event.calleridname.replace(/[_\s]+/g, ' '),
             state: event.channelstatedesc,
             timestamp: Date(),
         }
-        // console.log(mqtt_conf.trunk_prefix + '/' + trunk + ": " + JSON.stringify(tmp))
-        client.publish( mqtt_conf.trunk_prefix + '/' + trunk, JSON.stringify( tmp ) )
+        calls[event.linkedid] = tmp;
+
+        client.publish(mqtt_conf.trunk_prefix + '/' + trunk, JSON.stringify(tmp))
+    }
+    // Trunk Answer
+    else if (event.event === 'DialEnd' &&
+        callers[event.calleridnum] &&
+        calls[event.linkedid] &&
+        event.destchannelstatedesc === 'Up') {
+        var tmp = calls[event.linkedid]
+        var trunk = tmp.trunk.toLowerCase()
+        tmp.state = 'answer'
+        tmp.timestamp = Date()
+
+        client.publish(mqtt_conf.ext_prefix + '/' + ext, JSON.stringify(tmp))
+    }
+    // Trunk Hangup
+    else if (event.event === 'Hangup' &&
+        event.channelstatedesc === 'Up' &&
+        calls[event.linkedid] &&
+        callers[event.calleridnum]) {
+        var tmp = calls[event.linkedid]
+        var trunk = tmp.trunk.toLowerCase()
+        tmp.state = 'hangup'
+        tmp.timestamp = Date()
+
+        client.publish(mqtt_conf.trunk_prefix + '/' + trunk, JSON.stringify(tmp))
+
+        delete calls[event.linkedid]
+    }
     // Ring internal extensions
-    } else if ( event.event === 'DialBegin' &&
+    else if (event.event === 'DialBegin' &&
         event.channelstatedesc === 'Ring' &&
-        event.destcalleridname ) {
+        calls[event.linkedid] &&
+        event.destcalleridnum) {
         var ext = event.destcalleridnum
         var tmp = {
             to_num: event.destcalleridnum,
             from_num: event.calleridnum === 'Unavailable' ? '' : event.calleridnum,
-            from_name: callers[event.calleridnum] ? callers[event.calleridnum] : event.calleridname.replace( /[_\s]+/g, ' ' ),
-            state: event.channelstatedesc,
+            // from_name: callers[event.calleridnum] ? callers[event.calleridnum] : event.calleridname.replace( /[_\s]+/g, ' ' ),
+            from_name: calls[event.linkedid] ? calls[event.linkedid].from_name : event.calleridname.replace(/[_\s]+/g, ' '),
+            state: 'ring',
             timestamp: Date(),
         }
         // console.log(mqtt_conf.ext_prefix + '/' + ext + ": " + JSON.stringify(tmp))
-        client.publish( mqtt_conf.ext_prefix + '/' + ext, JSON.stringify( tmp ) )
+        client.publish(mqtt_conf.ext_prefix + '/' + ext, JSON.stringify(tmp))
     }
-} )
+    // Extension Answer
+    else if (event.event === 'DialEnd' &&
+        calls[event.linkedid] &&
+        event.destchannelstatedesc === 'Up') {
+        var ext = event.destcalleridnum
+        var tmp = {
+            to_num: event.destcalleridnum,
+            from_num: event.calleridnum === 'Unavailable' ? '' : event.calleridnum,
+            // from_name: callers[event.calleridnum] ? callers[event.calleridnum] : event.calleridname.replace( /[_\s]+/g, ' ' ),
+            from_name: calls[event.linkedid] ? calls[event.linkedid].from_name : event.calleridname.replace(/[_\s]+/g, ' '),
+            state: 'answer',
+            timestamp: Date(),
+        }
+        // console.log(mqtt_conf.ext_prefix + '/' + ext + ": " + JSON.stringify(tmp))
+        client.publish(mqtt_conf.ext_prefix + '/' + ext, JSON.stringify(tmp))
+    }
+    // Extension Hangup
+    else if (event.event === 'Hangup' &&
+        event.channelstatedesc === 'Up' &&
+        calls[event.linkedid] &&
+        event.calleridnum) {
+        var ext = event.calleridnum
+        var tmp = {
+            to_num: event.calleridnum,
+            from_num: event.connectedlinenum === 'Unavailable' ? '' : event.connectedlinenum,
+            // from_name: callers[event.connectedlinenum] ? callers[event.connectedlinenum] : event.connectedlinename.replace( /[_\s]+/g, ' ' ),
+            from_name: calls[event.linkedid] ? calls[event.linkedid].from_name : event.calleridname.replace(/[_\s]+/g, ' '),
+            state: 'hangup',
+            timestamp: Date(),
+        }
+        // console.log(mqtt_conf.ext_prefix + '/' + ext + ": " + JSON.stringify(tmp))
+        client.publish(mqtt_conf.ext_prefix + '/' + ext, JSON.stringify(tmp))
+    }
+})
 
-process.on( 'SIGTERM', function () {
-    nami.close( function () {
-        console.warn( "Exiting on SIGTERM" )
-    } )
-    process.exit( 1 )
-} )
-process.on( 'SIGINT', function () {
-    nami.close( function () {
-        console.warn( "Exiting on SIGINT" )
-    } )
-    process.exit( 1 )
-} )
+process.on('SIGTERM', function() {
+    nami.close(function() {
+        console.warn("Exiting on SIGTERM")
+    })
+    process.exit(1)
+})
+process.on('SIGINT', function() {
+    nami.close(function() {
+        console.warn("Exiting on SIGINT")
+    })
+    process.exit(1)
+})
 
 // MQTT Activity
 
-client.on( 'message', function ( topic, message ) {
+client.on('message', function(topic, message) {
     mqtt_activity = Date.now()
-} )
+})
 
 // AMI Keepalive
 
-setInterval( function () {
-    nami.send( new namiLib.Actions.Ping(), function ( response ) {
-        if ( response.ping !== 'Pong' ) {
+setInterval(function() {
+    nami.send(new namiLib.Actions.Ping(), function(response) {
+        if (response.ping !== 'Pong') {
             return
         }
         ami_activity = Date.now()
-    } )
-}, 60000 )
+    })
+}, 60000)
 
 // MQTT Keepalive
-setInterval( function () {
-    client.publish( mqtt_conf.ping_topic, JSON.stringify( {
+setInterval(function() {
+    client.publish(mqtt_conf.ping_topic, JSON.stringify({
         timestamp: Date()
-    } ) )
-}, 60000 )
+    }))
+}, 60000)
 
-setInterval( function () {
-    var ami_last = ( Date.now() - ami_activity ) / 1000.0
-    if ( ami_last >= 90 ) {
-        console.warn( "Exit due to AMI inactivity" )
-        process.exit( 10 )
+setInterval(function() {
+    var ami_last = (Date.now() - ami_activity) / 1000.0
+    if (ami_last >= 90) {
+        console.warn("Exit due to AMI inactivity")
+        process.exit(10)
     }
-    var mqtt_last = ( Date.now() - mqtt_activity ) / 1000.0
-    if ( mqtt_last >= 90 ) {
-        console.warn( "Exit due to MQTT inactivity" )
-        process.exit( 10 )
+    var mqtt_last = (Date.now() - mqtt_activity) / 1000.0
+    if (mqtt_last >= 90) {
+        console.warn("Exit due to MQTT inactivity")
+        process.exit(10)
     }
-}, 10000 )
+}, 10000)
 
-console.log( "Starting" )
+console.log("Starting")
 
 nami.open()
+
