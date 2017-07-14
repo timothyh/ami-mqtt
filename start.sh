5c0ef11b814934e41307e70fb1cecf7bf1a88731
Timothy Hunt <tim-github.com@timhunt.net>
2017-07-14 19:10:09 -0400

    Tweak startup script

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash --
 
-exec 2>&1 >> /tmp/calls.log < /dev/null
+# Sample startup script
+#
+# Will restart when needed
+#
+# exec 2>&1 >> /tmp/calls.log < /dev/null
 
 (
 while true ; do
 	nodejs ami-mqtt.js 
-	[ $? ne 10 ] && break
+	[ $? -ne 10 ] && break
 	sleep 5
 done
 ) &
